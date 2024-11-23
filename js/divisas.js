6925b4b1bfc47750a727ecb887feccd6a7a2525e
ᓚᘏᗢ michelle ! <96703919+fairyofshampoo@users.noreply.github.com>
2024-11-22 19:47:12 -0600

    Add Axios for API requests and refactor currency conversion logic

--- js/divisas.js
+++ js/divisas.js
@@ -3,62 +3,58 @@ const API_APP_ID = "e77f99c02f404d34a3631b67223d85e5";
 const API_MXN_CONCURRENCY = "MXN";
 var pesos_a_dolares = true;
 
-window.onload = function(){
+window.onload = function () {
     actualizarImagenesMonedas();
 }
 
-function intercambiarTipoConversion(){
+function intercambiarTipoConversion() {
     pesos_a_dolares = !pesos_a_dolares;
     actualizarImagenesMonedas();
 }
 
-function actualizarImagenesMonedas(){
+function actualizarImagenesMonedas() {
     var titulo = document.getElementById("h1_titulo");
     var img1 = document.getElementById("img1");
     var img2 = document.getElementById("img2");
 
-    if(pesos_a_dolares){
+    if (pesos_a_dolares) {
         titulo.innerText = "Conversor de Divisas Pesos Mexicanos a Dolares";
-        img1.src="img/mxn.png";
-        img2.src="img/usa.png";
+        img1.src = "img/mxn.png";
+        img2.src = "img/usa.png";
     } else {
-        img1.src="img/usa.png";
-        img2.src="img/mxn.png";
+        img1.src = "img/usa.png";
+        img2.src = "img/mxn.png";
     }
 }
 
-function convertir(){
+function convertir() {
     let importe = parseFloat(document.getElementById("txt_importe").value);
     let res = document.getElementById("txt_resultado");
     let txtasa = document.getElementById("txt_tasa");
-    console.log(pesos_a_dolares);
-    let request = new XMLHttpRequest();
-    request.open('GET', API_URL_BASE+"latest.json?app_id="+
-        API_APP_ID+"&symbols="+API_MXN_CONCURRENCY, true);
-    request.onerror = function(){
-        alert("No se puede consultar el API en este momento…");
-    }
 
-    request.onload = function(){
-        if(request.status >= 200 && request.status < 300){
-            var data = JSON.parse(this.response);
-            console.log(data);
-            var var_rates = data.rates;
-            var tasa = parseFloat (var_rates['MXN']);
-            if(!isNaN(importe) && importe > 0.0 && !isNaN(tasa) && tasa > 0.0){
-                txtasa.value = tasa;
-                if(pesos_a_dolares){
-                    res.value = (importe/tasa);
+    var URL_CONSULTA = API_URL_BASE + "latest.json?app_id=" + API_APP_ID + "&symbols=" + API_MXN_CONCURRENCY;
+
+    axios.get(URL_CONSULTA)
+        .then((response) => {
+            if (response.status >= 200 && response.status < 300) {
+                var data = response.data;
+                var var_rates = data.rates;
+                var tasa = parseFloat(var_rates['MXN']);
+                if (!isNaN(importe) && importe > 0.0 && !isNaN(tasa) && tasa > 0.0) {
+                    txtasa.value = tasa;
+                    if (pesos_a_dolares) {
+                        res.value = (importe / tasa);
+                    }
+                    else {
+                        res.value = (importe * tasa);
+                    }
                 }
-                else{
-                    res.value = (importe * tasa);
-                }
+            } else {
+                alert("No se pueden consultar las tasas de cambio")
             }
-        } else {
-            alert ("No se pueden consultar las tasas de cambio")
-        }
-    }
+        }).catch((error) => {
+            alert("No se puede consultar el API en este momento…");
+    });
 
-    request.send();
     return false;
 }
